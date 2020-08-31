803c8aec7768a0fe955ea65baf474f8568c8df03
SofiqurRahman <sofiqurr849@gmail.com>
2020-08-31 07:32:09 -0700

    first commit

---
+++ src/components/User/User.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const User = (props) => {
+  const familiar=props.familiar;
+  let greetings;
+  if (familiar) {
+    greetings = <p>Welcome, my friend.</p>
+  } else {
+    greetings = <p>Who the hell are you?</p>
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>Greetings</h2>
+        {greetings}
+      </div>
+      <div>
+        <h2>Food</h2>
+        {
+          familiar
+          ? <p>I will buy food for you.</p>
+          : <p>Lets eat his his whose whose.</p>
+        }
+      </div>
+      <div>
+        <h2>Connection</h2>
+        {/* <p>Let's join my facebook.</p>
+        <p>I don't add stranger?</p> */}
+        {
+          familiar && <p>Let's join my facebook.</p>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default User;
